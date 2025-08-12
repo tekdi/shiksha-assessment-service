1235d2c8484a7c2b529a8c7800e8feec556eeacd
Vinayak Patil <vinayak.patil@tekditechnologies.com>
2025-08-12 20:54:53 +0530

    Tenant Config

--- src/modules/configuration/configuration.module.ts
+++ src/modules/configuration/configuration.module.ts
@@ -3,14 +3,12 @@ import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigController } from './configuration.controller';
 import { ConfigurationService } from './configuration.service';
-import { TenantModule } from '@/common/tenant/tenant.module';
 import { CacheModule } from '../cache/cache.module';
 
 @Module({
   imports: [
     HttpModule, 
     ConfigModule, 
-    TenantModule,
     CacheModule
   ],
   controllers: [ConfigController],
